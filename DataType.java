d11efa2aee9c6cfe52ea110250a0f72f0805bce0
Sirivellasamuel <samuelsirivella@gmail.com>
2021-08-07 17:11:11 +0530

    initial commit

---
+++ DataType.java
@@ -0,0 +1,23 @@
+package com.cg.firstDay;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int it=12;
+		char ch= 'a';
+		float ft=0.09f;
+		boolean b1= false;
+		double db=0.0001;
+		byte byt=2;
+		System.out.println("int" +it);
+		System.out.println("char"+ch);
+		System.out.println("float"+ft);
+		System.out.println("boolean"+b1);
+		System.out.println("double"+db);
+		System.out.println("byte"+byt);
+		
+
+	}
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+package com.cg.firstDay;
+public class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		System.out.println("HieSamuel");
+	}
+}
